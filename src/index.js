cafdd11cee43e69e92bb48e725e72e079732497a
Atsushi Ishida <gipcompany@gmail.com>
2019-02-09 23:47:45 +0900

    install source-map

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom'
 import './style.css'
 import './style.scss'
 
-console.log('I am printing logs.')
-
 ReactDOM.render(
   <div>Hello, React!</div>,
   document.getElementById('root')
