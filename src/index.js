1a023bd6e1514acbbc981388a910687b9969abb5
Atsushi Ishida <gipcompany@gmail.com>
2019-02-09 12:37:20 +0900

    apply uglifyjs-webpack-plugin

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom'
 import './style.css'
 import './style.scss'
 
+console.log('I am printing logs.')
+
 ReactDOM.render(
   <div>Hello, React!</div>,
   document.getElementById('root')
